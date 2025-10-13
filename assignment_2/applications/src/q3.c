33bafeaafc5563a8bcc26e07b245c987ab7736f6
andreasp <dev@andreaspedersen.com>
2025-10-13 10:14:51 +0200

    finish q3

--- assignment_2/applications/src/q3.c
+++ assignment_2/applications/src/q3.c
@@ -10,9 +10,12 @@
 #define ARR_SIZE 1000000
 
 double arr[ARR_SIZE];
-double sum_parallel = 0.0;
 int num_threads;
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+typedef struct {
+    int thread_id;
+    double *partial_sums;
+} thread_arg_t;
 
 void *thread_func(void *arg);
 
@@ -23,7 +26,7 @@ void *thread_func(void *arg);
  */
 int main(int argc, char *argv[])
 {
-    // read user argument
+    /* === Read user argument === */
     if (argc != 2)
     {
         return 1;
@@ -35,25 +38,26 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    double sum_serial = 0;
-
-    // initialize array
+    /* === Initialize array === */
     srand(time(NULL));
     for (int i = 0; i < ARR_SIZE; i++)
     {
-        arr[i] = (double)rand()/(double)(RAND_MAX);
+        arr[i] = (double)rand() / (double)(RAND_MAX);
     }
 
-    // serial sum
+    /* === Start timer === */
     struct timeval tv;
     gettimeofday(&tv, NULL);
     unsigned long utime = 1000000 * tv.tv_sec + tv.tv_usec;
 
+    /* === Compute serial sum === */
+    double sum_serial = 0.0;
     for (int i = 0; i < ARR_SIZE; i++)
     {
         sum_serial += arr[i];
     }
 
+    /* === End timer and print results === */
     gettimeofday(&tv, NULL);
     unsigned long total_time = 1000000 * tv.tv_sec + tv.tv_usec - utime;
 
@@ -61,57 +65,73 @@ int main(int argc, char *argv[])
     printf("Serial sum:   %.6f,\tSerial time: %lu us\n", sum_serial, total_time);
     printf("\n--- Threads ---\n");
 
-    /* Create a pool of num_threads workers and keep them in workers */ 
+    /* Allocate memory for threads and their partial sums === */
     pthread_t *workers = malloc(sizeof(pthread_t) * num_threads);
+    double *partial_sums = malloc(sizeof(double) * num_threads);
 
-    // timer begin
+    /* === Start timer === */
     gettimeofday(&tv, NULL);
     utime = 1000000 * tv.tv_sec + tv.tv_usec;
 
+    /* === Generate thread IDs and start threads === */
     for (int i = 0; i < num_threads; i++)
     {
-        int *tid = malloc(sizeof(int));
-        *tid = i;
-        pthread_attr_t attr;
-        pthread_attr_init(&attr);
-        pthread_create(&workers[i], &attr, thread_func, tid); 
+        thread_arg_t *arg = malloc(sizeof(thread_arg_t));
+        arg->thread_id = i;
+        arg->partial_sums = partial_sums;
+        pthread_create(&workers[i], NULL, thread_func, arg); 
     }
     
+    /* === Wait until all threads are finished === */
     for (int i = 0; i < num_threads; i++)
     {
         pthread_join(workers[i], NULL);
     }
 
+    /* === Master computes sum from thread partial sums === */
+    double sum_parallel = 0.0;
+    for (int i = 0; i < num_threads; i++)
+    {
+        sum_parallel += partial_sums[i];
+    }
+
+    /* === End timer and print results === */
     gettimeofday(&tv, NULL);
     total_time = 1000000 * tv.tv_sec + tv.tv_usec - utime;
 
     printf("\n--- Thread Sum ---\n");
     printf("Parallel Sum: %.6f,\tParallel time: %lu us\n", sum_parallel, total_time);
 
+    /* === Free resources === */
     free(workers);
 
     return 0;
 }
 
-void *thread_func(void *arg) { 
-    // assign each thread an id so that they are unique in range [0, num_thread - 1 ]
-    int my_id = *(int *)arg;
+void *thread_func(void *arg)
+{ 
+    /* === Parse thread arg to get ID and partial sum pointer === */
+    thread_arg_t *data = (thread_arg_t *)arg;
+    int my_id = data->thread_id;
+    double *partial_sums = data->partial_sums;
     free(arg);
 
+    /* === Compute part of array to sum === */
     int chunk_size = ARR_SIZE / num_threads;
     int start = my_id * chunk_size;
     int end = (my_id == num_threads - 1) ? ARR_SIZE : start + chunk_size;
 
-    double my_sum = 0.0;
+    /* === Do sum === */
+    double thread_sum = 0.0;
     for (int i = start; i < end; i++)
     {
-        my_sum += arr[i];
+        thread_sum += arr[i];
     }
 
-    pthread_mutex_lock(&lock);
-    sum_parallel += my_sum;
-    pthread_mutex_unlock(&lock);
+    /* === Write partial sum so it is available to master === */
+    partial_sums[my_id] = thread_sum;
 
-    printf("Thread %d sum = %f\n", my_id, my_sum);
+    /* === Print thread result and exit === */
+    printf("Thread %d sum = %f\n", my_id, thread_sum);
     pthread_exit(0);
 }
